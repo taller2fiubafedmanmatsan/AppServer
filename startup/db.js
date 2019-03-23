ca01455476466fcd04f6ce752de14dd2aeb6749a
llmanuel <manuel.llauro@restorando.com>
2019-03-23 18:35:20 -0300

    revert some changes an try not to buffer mongoose

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,8 @@ const config = require('config');
 
 module.exports = function() {
   const db = config.get('db');
-  mongoose.connect(db.host, db.name, db.port, db.options)
+  mongoose.set('bufferCommands', false);
+  mongoose.connect(db, {useNewUrlParser: true})
       .then(() => dbLog(`Connected to ${db}`))
       .catch((ex) => dbLog(`An error occured while connecting to ${db}.`, ex));
 };
