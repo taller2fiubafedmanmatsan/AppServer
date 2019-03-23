c08e8ed5379774b0101d2c2c27c47c36e49947c8
llmanuel <manuel.llauro@restorando.com>
2019-03-23 17:34:49 -0300

    adding token attribute to user model

--- models/user.js
+++ models/user.js
@@ -5,8 +5,9 @@ const Joi = require('joi');
 const userSchema = mongoose.Schema({
   name: {type: String, minlenght: 1, maxlenght: 50, require: true},
   email: {type: String, require: true},
-  nickname: String,
-  password: String,
+  nickname: {type: String, require: true},
+  password: {type: String, require: true},
+  token: String,
   isAdmin: Boolean
 });
 
@@ -17,7 +18,8 @@ function validateUser(user) {
     name: Joi.string().min(1).max(50).required(),
     email: Joi.string().required(),
     nickname: Joi.string(),
-    password: Joi.string().min(6).max(30).required()
+    password: Joi.string().min(6).max(30).required(),
+    isAdmin: Joi.bool().required()
   };
 
   return Joi.validate(user, schema);
