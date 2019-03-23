c1bbf39269f3e77e22c2b9167986770e9b2f5fe5
llmanuel <manuel.llauro@restorando.com>
2019-03-23 15:59:09 -0300

    adding eslinter configuration and make linter corrections in all files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('./startup/routes')(app);
 require('./startup/db')();
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => { console.log(`Listening on port ${port}...`) });
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
 
 module.exports = server;
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const { User, validate } = require('../models/user');
+const {User, validate} = require('../models/user');
 
 router.get('/:id', async (request, response) => {
   const user = await User.findById(request.params.id).select('-password');
-  if (!user) return response.status(404).send(`The user by id: ${request.params.id} doesn't exists.`);
+  const msg = `The user by id: ${request.params.id} doesn't exists.`;
+  if (!user) return response.status(404).send(msg);
 
   response.send(user);
 });
 
 router.post('/', async (request, response) => {
-  const { error } = validate(request.body);
+  const {error} = validate(request.body);
   if (error) return response.status(400).send(error.details[0].message);
 
-  const { name, email, password, isAdmin } = request.body;
+  const {name, email, password, isAdmin} = request.body;
 
-  let user = await User.findOne({ email });
+  let user = await User.findOne({email});
   if (user) return response.status(400).send('User already registered.');
 
-  user = new User({ name, email, password, isAdmin });
+  user = new User({name, email, password, isAdmin});
 
-  const new_user = await user.save();
+  const newUser = await user.save();
 
-  response.status(200).send(`New user ${new_user.name} created`);
+  response.status(200).send(`New user ${newUser.name} created`);
 });
 
 module.exports = router;
