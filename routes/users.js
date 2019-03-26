1635c521104d40d34cbe8f475b28efdba5259a7e
llmanuel <manuel.llauro@restorando.com>
2019-03-26 00:58:36 -0300

    adding swagger to proyect

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const dbLog = require('debug')('app:dbLog');
 const router = express.Router();
 
 const {User, validate} = require('../models/user');
 
 router.get('/:id', async (request, response) => {
-  const user = await User.findById(request.params.id).select('-password');
+  const user = await User.findById(request.params.id).select('-password -__v');
   const msg = `The user by id: ${request.params.id} doesn't exists.`;
   if (!user) return response.status(404).send(msg);
 
@@ -17,15 +16,15 @@ router.post('/', async (request, response) => {
   if (error) return response.status(400).send(error.details[0].message);
 
   const {name, email, password, isAdmin} = request.body;
-  dbLog(`Por preguntar si hay un user`);
+
   let user = await User.findOne({email});
-  dbLog(`Pregunte!`);
+
   if (user) return response.status(400).send('User already registered.');
 
   user = new User({name, email, password, isAdmin});
-  dbLog(`Por hacer save!`);
+
   const newUser = await user.save();
-  dbLog(`save loco!`);
+
   response.status(200).send(`New user ${newUser.name} created`);
 });
 
