900d88b0e1150512217d95af4013167a7f83df38
llmanuel <manuel.llauro@restorando.com>
2019-03-27 21:40:44 -0300

    adding auth route

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const users = require('../routes/users');
 const rootFile = require('../routes/root');
+const auth = require('../routes/auth');
 const swaggerUi = require('swagger-ui-express');
 const read = require('read-yaml');
 const swaggerDocument = read.sync('./swagger.yaml');
@@ -10,4 +11,5 @@ module.exports = function(app) {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/', rootFile);
   app.use('/api/users', users);
+  app.use('/api/auth', auth);
 };
