deba912f8dff8bf678008ea06cb1d2eee5556cde
llmanuel <manuel.llauro@restorando.com>
2019-03-27 21:41:17 -0300

    refactor of user model

--- models/user.js
+++ models/user.js
@@ -1,25 +1,38 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const userSchema = mongoose.Schema({
   name: {type: String, minlenght: 1, maxlenght: 50, require: true},
-  email: {type: String, require: true},
-  nickname: {type: String, require: true},
-  password: {type: String, require: true},
-  token: String,
+  email: {
+    type: String,
+    minlenght: 3,
+    maxlenght: 50,
+    require: true,
+    unique: true
+  },
+  nickname: {type: String, minlenght: 1, maxlenght: 50, require: true},
+  password: {type: String, minlenght: 6, maxlenght: 255, require: true},
   isAdmin: Boolean
 });
+
+userSchema.methods.getAuthToken = function() {
+  return jwt.sign({_id: this._id, name: this.name, isAdmin: this.isAdmin},
+      config.get('jwt_key'),
+      {expiresIn: 86400} // expires in 24 hours
+  );
+};
 
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(1).max(50).required(),
-    email: Joi.string().required(),
-    nickname: Joi.string(),
-    password: Joi.string().min(6).max(30).required(),
-    isAdmin: Joi.bool().required()
+    email: Joi.string().min(3).max(50).required().email(),
+    nickname: Joi.string().min(1).max(50),
+    password: Joi.string().min(6).max(255).required(),
+    isAdmin: Joi.bool()
   };
 
   return Joi.validate(user, schema);
