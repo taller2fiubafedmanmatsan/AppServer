45aad13a39d25f327d94ce305cd4bf281df8db75
Santiago <pinto.santiago.augusto@gmail.com>
2019-03-31 19:58:45 -0300

    Agregue servicio de mail para restaurar la clave de un usuario

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,15 @@ const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const router = express.Router();
 const auth = require('../middelware/auth');
+const mailer = require('../mailer/mail');
+const randomstring = require('randomstring');
 
-const {User, validate, validateUpdate} = require('../models/user');
+const {
+  User,
+  validate,
+  validateUpdate,
+  validatePasswordRestore
+} = require('../models/user');
 
 router.get('/me', auth, async (request, response) => {
   const user = await User.findById(request.user._id)
@@ -49,13 +56,34 @@ router.put('/me', auth, async (request, response) => {
     request.body.password = await bcrypt.hash(request.body.password, salt);
   };
 
-  const user = await User.findByIdAndUpdate(user._id, _.pick(request.body,
+  user = await User.findByIdAndUpdate(request.user._id, _.pick(request.body,
       [
         'nickname', 'password', 'photo_url'
       ]
   ));
 
   response.status(200).send(_.pick(user, ['name', 'email']));
+});
+
+
+router.post('/restorepassword', async (request, response) => {
+  const {error} = validatePasswordRestore(request.body);
+  if (error) return response.status(400).send(error.details[0].message);
+
+  const email = request.body.email;
+  const user = await User.findOne({email: email});
+
+  if (!user) return response.status(400).send('Invalid email');
+
+  const newPassword = randomstring.generate(10);
+  const salt = await bcrypt.genSalt(10);
+  request.body.password = await bcrypt.hash(newPassword, salt);
+  await User.findByIdAndUpdate(user._id, _.pick(request.body,
+      [
+        'password'
+      ]
+  ));
+  mailer.sendMail(request, response, newPassword);
 });
 
 
