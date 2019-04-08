69fedbc6e52102ccfe59a4c57a9fc6552ecbcc55
llmanuel <manuel.llauro@restorando.com>
2019-04-08 11:34:57 -0300

    fix typos in users test

--- tests/integration/routes/users.test.js
+++ tests/integration/routes/users.test.js
@@ -18,6 +18,7 @@ describe('/api/users', ()=> {
     let name;
     let email;
     let password;
+    let token;
 
     const execute = ()=> {
       return request(server)
@@ -50,110 +51,109 @@ describe('/api/users', ()=> {
       expect(response.body.email).toBe(email);
     });
   });
+
+  describe('POST /', ()=> {
+    let name;
+    let email;
+    let password;
+
+    const execute = ()=> {
+      return request(server)
+          .post('/api/users')
+          .send({name, email, password});
+    };
+
+    beforeEach(async ()=> {
+      name = 'name';
+      email = 'user@test.com';
+      password = 'password';
+      await User.remove({});
+    });
+
+    it('should return 400 if user name is missing', async ()=> {
+      name = null;
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if user name is less than 1 characters', async ()=> {
+      name = '';
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if user name is more than 50 characters', async ()=> {
+      name = new Array(52).join('a');
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if user email is missing', async ()=> {
+      email = null;
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if email is less than 5 characters', async ()=> {
+      email = 'abc';
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if email is more than 50 characters', async ()=> {
+      email = new Array(52).join('a');
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if user email is not a valid email', async ()=> {
+      email = 'abcdefg';
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if user password is missing', async ()=> {
+      password = null;
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if password is less than 6 characters', async ()=> {
+      password = 'abcde';
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if password is more than 255 characters', async ()=> {
+      password = new Array(257).join('a');
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return 400 if the user is already registered', async ()=> {
+      await execute();
+      const response = await execute();
+
+      expect(response.status).toBe(400);
+    });
+
+    it('should return new user if request is valid', async ()=> {
+      const response = await execute();
+
+      expect(response.status).toBe(200);
+      expect(Object.keys(response.body)).toEqual(
+          expect.arrayContaining(['name', 'email'])
+      );
+    });
+  });
 });
-
-describe('POST /', ()=> {
-  let name;
-  let email;
-  let password;
-
-  const execute = ()=> {
-    return request(server)
-        .post('/api/users')
-        .send({name, email, password});
-  };
-
-  beforeEach(async ()=> {
-    name = 'name';
-    email = 'user@test.com';
-    password = 'password';
-    await User.remove({});
-  });
-
-  it('should return 400 if user name is missing', async ()=> {
-    name = null;
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if user name is less than 1 characters', async ()=> {
-    name = '';
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if user name is more than 50 characters', async ()=> {
-    name = new Array(52).join('a');
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if user email is missing', async ()=> {
-    email = null;
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if email is less than 5 characters', async ()=> {
-    email = 'abc';
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if email is more than 50 characters', async ()=> {
-    email = new Array(52).join('a');
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if user email is not a valid email', async ()=> {
-    email = 'abcdefg';
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if user password is missing', async ()=> {
-    password = null;
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if password is less than 6 characters', async ()=> {
-    password = 'abcde';
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if password is more than 255 characters', async ()=> {
-    password = new Array(257).join('a');
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return 400 if the user is already registered', async ()=> {
-    await execute();
-    const response = await execute();
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should return new user if request is valid', async ()=> {
-    const response = await execute();
-
-    expect(response.status).toBe(200);
-    expect(Object.keys(response.body)).toEqual(
-        expect.arrayContaining(['name', 'email'])
-    );
-  });
-});
-
