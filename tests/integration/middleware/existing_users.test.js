3aa9ce339315cba0807b0c1e69249fba9be3db7f
llmanuel <manuel.llauro@restorando.com>
2019-04-09 17:02:48 -0300

    create defenitive teest for existing users middleware

---
+++ tests/integration/middleware/existing_users.test.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const {User} = require('../../../models/user');
+
+let server;
+
+describe('existing_users middleware', ()=> {
+  beforeEach(()=> {
+    server = require('../../../index');
+  });
+
+  afterEach(async ()=> {
+    await User.remove({});
+    await server.close();
+  });
+
+  describe('main function', ()=> {
+    const data1 = {name: 'test', email: 'test@testing.com', pass: '123456'};
+    const data2 = {name: 'test2', email: 'test2@testing.com', pass: '123456'};
+    const data3 = {name: 'test3', email: 'test3@testing.com', pass: '123456'};
+    let token;
+
+    function createUser(name, email, pass) {
+      return request(server)
+          .post('/api/users')
+          .send({name: name, email: email, password: pass});
+    }
+
+    beforeEach(async ()=> {
+      await createUser(data1.name, data1.email, data1.pass);
+      await createUser(data2.name, data2.email, data2.pass);
+      // await createUser(data3.name, data3.email, data3.pass);
+
+      user = await User.findOne({name: 'test'});
+      token = user.getAuthToken();
+    });
+
+    const body = {
+      creator: data1.email,
+      users: [data1.email, data2.email, data3.email],
+      admins: [data1.email, data2.email, data3.email]
+    };
+
+    const execute = ()=> {
+      return request(server)
+          .post('/api/workspaces')
+          .set('x-auth-token', token)
+          .send(body);
+    };
+
+    it(`should return 404 when a user doesn't exist`, async ()=> {
+      const response = await execute();
+
+      expect(response.status).toEqual(404);
+      expect(response.text).toEqual('Invalid users.');
+    });
+  });
+});
