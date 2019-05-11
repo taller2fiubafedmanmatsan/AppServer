97a0995333bb965208b1eb6942a36b338a1dde32
llmanuel <manuel.llauro@restorando.com>
2019-05-11 13:36:56 -0300

    fix firebase error

--- startup/firebase.js
+++ startup/firebase.js
@@ -1,19 +1,8 @@
 // Integración con firebase
-// PD: Es tremendamente inseguro esto pero bueno
 const admin = require('firebase-admin');
 const config = require('config');
 
 function setFirebaseKey() {
-  console.log(config.get('fireb_type'));
-  console.log(config.get('fireb_project_id'));
-  console.log(config.get('fireb_private_key_id'));
-  console.log(config.get('fireb_private_key'));
-  console.log(config.get('fireb_client_email'));
-  console.log(config.get('fireb_client_id'));
-  console.log(config.get('fireb_auth_uri'));
-  console.log(config.get('fireb_token_uri'));
-  console.log(config.get('fireb_auth_provider'));
-  console.log(config.get('fireb_client_url'));
   const firebaseKey = {
     type: `${config.get('fireb_type')}`,
     project_id: `${config.get('fireb_project_id')}`,
@@ -31,13 +20,9 @@ function setFirebaseKey() {
 };
 
 module.exports = function() {
-  console.log('hello');
   if (process.env.NODE_ENV != 'test') {
-    console.log('empiezo a buscar firebase');
-    const firebasKey = setFirebaseKey();
-    console.log(firebasKey);
     admin.initializeApp({
-      credential: admin.credential.cert(firebasKey),
+      credential: admin.credential.cert(setFirebaseKey()),
       databaseURL: config.get('firebase_database')
     });
   }
