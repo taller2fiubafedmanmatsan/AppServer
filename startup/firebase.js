a8f881d5de08856fa7428cabaaf7b93fa3d1a7e4
llmanuel <manuel.llauro@restorando.com>
2019-05-11 12:50:53 -0300

    test

--- startup/firebase.js
+++ startup/firebase.js
@@ -7,7 +7,7 @@ module.exports = function() {
   console.log('hello');
   if (process.env.NODE_ENV != 'test') {
     console.log('empiezo a buscar firebase');
-    const firebasKey = JSON.parse(config.get('firebase_key'));
+    const firebasKey = config.get('firebase_key');
     console.log(firebaseKey);
     admin.initializeApp({
       credential: admin.credential.cert(firebasKey),
