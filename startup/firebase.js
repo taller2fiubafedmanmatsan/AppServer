5114f3fb6158ebcd6e648a91ea019ace5519ce18
llmanuel <manuel.llauro@restorando.com>
2019-05-11 13:05:47 -0300

    set direct env variables

--- startup/firebase.js
+++ startup/firebase.js
@@ -3,11 +3,26 @@
 const admin = require('firebase-admin');
 const config = require('config');
 
+const setFirebaseKey = () => {
+  return {
+    type: config.get('fireb_type'),
+    project_id: config.get('fireb_project_id'),
+    private_key_id: config.get('fireb_private_key_id'),
+    private_key: config.get('fireb_private_key'),
+    client_email: config.get('fireb_client_email'),
+    client_id: config.get('fireb_client_id'),
+    auth_uri: config.get('fireb_auth_uri'),
+    token_uri: config.get('fireb_token_uri'),
+    auth_provider_x509_cert_url: config.get('fireb_auth_provider'),
+    client_x509_cert_url: config.get('fireb_client_url')
+  };
+};
+
 module.exports = function() {
   console.log('hello');
   if (process.env.NODE_ENV != 'test') {
     console.log('empiezo a buscar firebase');
-    const firebasKey = config.get('firebase_key');
+    const firebasKey = setFirebaseKey();
     console.log(firebaseKey);
     admin.initializeApp({
       credential: admin.credential.cert(firebasKey),
