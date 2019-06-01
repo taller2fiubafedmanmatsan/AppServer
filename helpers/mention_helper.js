d845311e969fbc3abbfc47194f20cbdd967314ab
Santiago <pinto.santiago.augusto@gmail.com>
2019-06-01 20:31:20 -0300

    Fixed problem with https request that was not wel configured

--- helpers/mention_helper.js
+++ helpers/mention_helper.js
@@ -1,24 +1,35 @@
-const http = require('http');
+const https = require('https');
 const {User} = require('../models/user');
+
+
+function parseUrl(url) {
+  const protocol = 'https://';
+  const urlEnd = url.indexOf('/', protocol.length);
+  return url.substr(protocol.length, urlEnd - protocol.length);
+};
+
 
 module.exports.handleMentions =
   async function(workspace, channel, message, sender) {
     const atPos = message.text.indexOf('@');
     if (atPos < 0) return;
     const spacePos = message.text.indexOf(' ', atPos);
-    const name = message.text.substr(atPos + 1, spacePos - atPos);
-    console.log(name);
-    const user = User.findOne({name: name});
+    const name = message.text.substr(atPos + 1, spacePos - atPos - 1);
+    const user = await User.findOne({name: name});
 
     if (!user) return;
 
     if (user.url) { // El usuario es un bot si la url esta seteada
-      const body = {
-        sender: sender.name,
-        message: message.text,
-        channel: channel.name,
-        workspace: workspace.name
+      const options = {
+        hostname: parseUrl(user.url),
+        method: 'POST',
+        body: {
+          sender: sender.name,
+          message: message.text,
+          channel: channel.name,
+          workspace: workspace.name
+        }
       };
-      http.request(bot.url, body);
+      https.request(options);
     }
   };
